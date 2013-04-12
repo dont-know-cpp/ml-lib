4ca93412b1e3e76d5480b80911c7cfc02a5cd4bc
JJ <hmmm@hmm.hmm>
2013-04-11 23:43:48 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <conv_neural_network.hpp>
+
+int main()
+{
+  //using namespace conv_neural_network;
+  
+  //neural_network cnn;
+
+  //cnn.add_layer(INPUT_LAYER, 1, 29, 0, 0);
+  /* 5x5 kernel, 6 feature maps, sample every other pixel */
+  //cnn.add_layer(CONV_LAYER, 6, 13, 5, 2);
+
+  printf("%f\n", RANDOM_PLUS_MINUS_ONE);
+ 
+  return 0;
+}
